1b3c2c05bd97320c1c3ac2f1100925d98c3a3289
deeps96 <leonardo.huebscher.business@gmail.com>
2018-02-08 11:50:51 +0100

    Add the route to the controller. #9

--- src/main/java/de/hpi/parser/controller/ParserController.java
+++ src/main/java/de/hpi/parser/controller/ParserController.java
@@ -1,11 +1,12 @@
 package de.hpi.parser.controller;
 
 import de.hpi.parser.service.ParserService;
+import de.hpi.restclient.dto.ParseOfferParameter;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ParserController {
@@ -17,4 +18,8 @@ public class ParserController {
         setService(service);
     }
 
+    @RequestMapping(value = "/parseOffer", method = RequestMethod.POST,  produces = "application/json")
+    public void parseOffer(@RequestBody ParseOfferParameter parameter){
+        System.out.println(getService().parseOffer(parameter.getHtml(), parameter.getShopID()));
+    }
 }
--- src/main/java/de/hpi/parser/service/ParserService.java
+++ src/main/java/de/hpi/parser/service/ParserService.java
@@ -1,8 +1,13 @@
 package de.hpi.parser.service;
 
+import de.hpi.restclient.pojo.ExtractedDataMap;
+import de.hpi.restclient.pojo.Rules;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ParserService {
 
+    public ExtractedDataMap parseOffer(String html, long shopID) {
+        return null;
+    }
 }
