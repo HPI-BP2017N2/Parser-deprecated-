a2ea085e6a7d77ae6f6af82fd2655e24df3c69a1
deeps96 <leonardo.huebscher.business@gmail.com>
2018-02-08 12:56:47 +0100

    Created rules callback route. #9

--- src/main/java/de/hpi/parser/controller/ParserController.java
+++ src/main/java/de/hpi/parser/controller/ParserController.java
@@ -1,6 +1,7 @@
 package de.hpi.parser.controller;
 
 import de.hpi.parser.service.ParserService;
+import de.hpi.restclient.dto.GetRulesResponse;
 import de.hpi.restclient.dto.ParseOfferParameter;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -18,8 +19,13 @@ public class ParserController {
         setService(service);
     }
 
-    @RequestMapping(value = "/parseOffer", method = RequestMethod.POST,  produces = "application/json")
+    @RequestMapping(value = "/parseOffer", method = RequestMethod.POST)
     public void parseOffer(@RequestBody ParseOfferParameter parameter){
-        System.out.println(getService().parseOffer(parameter.getHtml(), parameter.getShopID()));
+        getService().parseOffer(parameter.getHtml(), parameter.getShopID());
+    }
+
+    @RequestMapping(value = "/rulesCallback", method = RequestMethod.POST)
+    public void rulesCallback(@RequestBody GetRulesResponse parameter){
+        getService().handleRulesResponse(parameter.getRules(), parameter.getShopID());
     }
 }
--- src/main/java/de/hpi/parser/service/ParserService.java
+++ src/main/java/de/hpi/parser/service/ParserService.java
@@ -1,13 +1,41 @@
 package de.hpi.parser.service;
 
+import de.hpi.parser.properties.ParserProperties;
+import de.hpi.restclient.clients.BPBridgeClient;
+import de.hpi.restclient.clients.ShopRulesGeneratorClient;
 import de.hpi.restclient.pojo.ExtractedDataMap;
 import de.hpi.restclient.pojo.Rules;
+import de.hpi.restclient.properties.BPBridgeProperties;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+
+@Getter(AccessLevel.PRIVATE)
+@Setter(AccessLevel.PRIVATE)
 @Service
 public class ParserService {
 
-    public ExtractedDataMap parseOffer(String html, long shopID) {
-        return null;
+    private HashMap<Long, String> tmpHTMLFiles;
+    private ParserProperties properties;
+    private ShopRulesGeneratorClient shopRulesGeneratorClient;
+
+    @Autowired
+    public ParserService(ParserProperties properties, ShopRulesGeneratorClient shopRulesGeneratorClient) {
+        setProperties(properties);
+        setTmpHTMLFiles(new HashMap<Long, String>());
+        setShopRulesGeneratorClient(shopRulesGeneratorClient);
+    }
+
+    public void parseOffer(String html, long shopID) {
+        getTmpHTMLFiles().put(shopID, html);
+        getShopRulesGeneratorClient().getRules(shopID, getProperties().getRoot(), getProperties().getRulesCallbackRoute());
+    }
+
+    public void handleRulesResponse(Rules rules) {
+
     }
 }
