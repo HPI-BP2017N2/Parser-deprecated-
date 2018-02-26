9b2cd49a40df0f1f0511a1f3f24ba44c7e8750e0
dey4ss <lindnerdaniel@yahoo.de>
2018-02-26 13:54:29 +0100

    fixed wrong dto

--- src/main/java/de/hpi/parser/service/ParserService.java
+++ src/main/java/de/hpi/parser/service/ParserService.java
@@ -3,22 +3,16 @@ package de.hpi.parser.service;
 import de.hpi.parser.model.Parser;
 import de.hpi.parser.properties.ParserProperties;
 import de.hpi.parser.respository.ParsedOfferRepository;
-import de.hpi.restclient.clients.BPBridgeClient;
 import de.hpi.restclient.clients.ShopRulesGeneratorClient;
 import de.hpi.restclient.dto.GetRulesResponse;
-import de.hpi.restclient.dto.ParsedOffer;
 import de.hpi.restclient.pojo.ExtractedDataEntry;
 import de.hpi.restclient.pojo.ExtractedDataMap;
 import de.hpi.restclient.pojo.OfferAttribute;
-import de.hpi.restclient.pojo.Rules;
-import de.hpi.restclient.properties.BPBridgeProperties;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.HashMap;
 
 @Getter(AccessLevel.PRIVATE)
 @Setter(AccessLevel.PRIVATE)
